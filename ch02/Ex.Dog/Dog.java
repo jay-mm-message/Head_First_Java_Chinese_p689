b7c15a3004820ca772495eed85eb9bc6a7c2310e
jcwang <jcwang@sonicwall.com>
2022-03-05 20:25:50 +0800

    Update Dog and TestDog.

--- ch02/Ex.Dog/Dog.java
+++ ch02/Ex.Dog/Dog.java
@@ -1,6 +1,35 @@
+
+
 public class Dog {
+
     private final String TAG="\nDog ";
+
+    private int size;
+    private String name;
+
+    public void setSize(int s) { size = s; }
+    public int getSize() { return size; }
+
+    public void setName(String n) { name = n; }
+    public String getName() { return name; }
+
     public Dog() {
         System.out.println(TAG + "Constructor");
+    
+        size = 0;
+        name = null;
+    }
+
+    void bark() {
+        if (size >= 5) {
+            System.out.println("WOW WOW");
+        } else {
+            System.out.println("wow wow");
+        }
+    }
+
+    void info() {
+        System.out.println("size: " + size
+                        +  "\nname: " + name);
     }
 }
--- ch02/Ex.Dog/TestDog.java
+++ ch02/Ex.Dog/TestDog.java
@@ -3,6 +3,15 @@ public class TestDog {
         
         Dog dog = new Dog();
     
+        dog.setName("ellen");
+        dog.setSize(5);
+        dog.info();
+        dog.bark();
 
+        Dog dog2 = new Dog();
+        dog2.setName("jay");
+        dog2.setSize(2);
+        dog2.info();
+        dog2.bark();
     }
 }
